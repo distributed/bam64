1beae53eb6b294d44122f8c12f51fda2f87c1087
Michael Meier <distributed@gloenggi.ch>
2011-10-07 00:53:49 +0200

    export bam64 state

--- bam64.h
+++ bam64.h
@@ -8,4 +8,11 @@ typedef bam64image bam64images[2];
 
 volatile extern uint8_t bam64flags[BAM64NUMFLAGS];
 
+
+volatile extern uint8_t bam64_to;
+volatile extern uint8_t bam64_bitnum;
+volatile extern uint8_t bam64_colnum;
+volatile extern uint8_t bam64_colpattern;
+volatile extern uint8_t *bam64_front;
+volatile extern uint8_t *bam64_shadow;
 #endif
