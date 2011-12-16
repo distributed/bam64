52e09b262ddd1764af77975ed8b20938b25c5f7e
Michael Meier <flowmaster@gloenggi.ch>
2011-12-16 16:33:20 +0100

    add flags FRMCNT and COPYFCNT

--- bam64defs.h
+++ bam64defs.h
@@ -2,8 +2,11 @@
 #define BAM64DEFS_H
 
 // pay attention to update BAM64NUMFLAGS!
-#define BAM64FLAG_COPY     0
-#define BAM64FLAG_COPIED   1
-#define BAM64NUMFLAGS      2
+#define BAM64FLAG_COPY     0   // request buffer swap. app -> ISR
+#define BAM64FLAG_COPIED   1   // buffer swap done. ISR -> app
+#define BAM64FLAG_FRMCNT   2   // 8 bit frame counter. to whom it may concern
+#define BAM64FLAG_COPYFCNT 3   // FRMCNT of first frame after last buffer swap.
+                               // to whom it may concern
+#define BAM64NUMFLAGS      4   // number of flag bytes for space allocation
 
 #endif
