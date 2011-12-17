92fdcf9240e8492e2ca1a7f9b17cb2b60d1f596e
Michael Meier <distributed@gloenggi.ch>
2011-12-17 12:14:26 +0100

    clean up bam64 configuration. add bam64config.h every application using bam64 needs to provide its own bam64config.h

---
+++ bam64config.h
@@ -0,0 +1,11 @@
+#ifndef BAM64CONFIG_H
+#define BAM64CONFIG_H
+
+#warning This is the default bam64 configuration file. If you are not
+#warning compiling the bam64 test bench, this is probably NOT what you want.
+
+#define BAM64NUMFAST 5
+#define BAM64LSBCYCLES 7
+
+
+#endif
